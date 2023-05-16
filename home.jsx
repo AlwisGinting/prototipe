6b585b5abe20d59f0f9dfc97260038c7fe8c3385
Alwis Ginting <123973362+AlwisGinting@users.noreply.github.com>
2023-05-16 16:39:23 +0700

    Add files via upload

---
+++ home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './main.css';
+
+const App = () => {
+  return (
+    <div className="v2_23">
+      <div className="v2_24">
+        <div className="v2_25">
+          <div className="v2_26">
+            <div className="v2_101"></div>
+            <div className="v2_102"></div>
+            <div className="v2_103"></div>
+            <div className="v2_104"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
